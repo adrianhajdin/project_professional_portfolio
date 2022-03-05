4e3b75d2d72c3833153cbbf0faad83f21e6c209b
Sreeramsupers <79449377+Sreeramsupers@users.noreply.github.com>
2022-03-05 20:09:03 +0530

    Update Footer.jsx

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -6,34 +6,54 @@ import { client } from '../../client';
 import './Footer.scss';
 
 const Footer = () => {
-  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
-  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-  const [loading, setLoading] = useState(false);
+ 	const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+	const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [danger, setDanger] = useState(false);
 
-  const { username, email, message } = formData;
+	const [emailError, setEmailError] = useState('');
+	const { name, email, message } = formData;
 
-  const handleChangeInput = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+	const handleChangeInput = (e) => {
+		const { name, value } = e.target;
+		setFormData({ ...formData, [name]: value });
+		setDanger(false);
+	};
 
-  const handleSubmit = () => {
-    setLoading(true);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setLoading(true);
 
-    const contact = {
-      _type: 'contact',
-      name: formData.username,
-      email: formData.email,
-      message: formData.message,
-    };
+		const contact = {
+			_type: 'contact',
+			name: formData.name,
+			email: formData.email,
+			message: formData.message,
+		};
 
-    client.create(contact)
-      .then(() => {
-        setLoading(false);
-        setIsFormSubmitted(true);
-      })
-      .catch((err) => console.log(err));
-  };
+		if (formData.name == '' || formData.message == '' || formData.email == '') {
+			setLoading(false);
+			setDanger(true);
+			return;
+		}
+
+		var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+		if (!formData.email.match(mailformat)) {
+			setLoading(false);
+			setEmailError('Email id is invalid');
+			setDanger(true);
+			return;
+		}
+
+		client
+			.create(contact)
+			.then(() => {
+				setLoading(false);
+				setIsFormSubmitted(true);
+			})
+			.catch((err) => console.log(err));
+	};
 
   return (
     <>
@@ -50,52 +70,52 @@ const Footer = () => {
         </div>
       </div>
       {!isFormSubmitted ? (
-				<form className='app__footer-form app__flex'>
-					<div className='app__flex'>
-						<input
-							className='p-text'
-							name='name'
-							type='text'
-							placeholder='Your name'
-							value={name}
-							onChange={handleChangeInput}
-							required
-						/>
-					</div>
-					<div className='app__flex'>
-						<input
-							className={`${danger ? 'danger' : 'p-text'}`}
-							name='email'
-							type='email'
-							placeholder='Your email'
-							value={email}
-							onChange={handleChangeInput}
-							required
-						/>
-						{emailError && danger && (
-							<div className='app__flex'>
-								<p className='text-danger'>{emailError}</p>
-							</div>
-						)}
-					</div>
-					<div>
-						<textarea
-							name='message'
-							value={message}
-							onChange={handleChangeInput}
-							placeholder='Your Message'
-							className='p-text'
-							required
-						/>
-					</div>
-
-					<button type='submit' className='p-text' onClick={handleSubmit}>
-						{!loading ? 'Send Message' : 'Sending...'}
-					</button>
-				</form>
-			) : (
-				<h3 className='thankyou'>Thankyou for getting in touch</h3>
+	<form className='app__footer-form app__flex'>
+		<div className='app__flex'>
+			<input
+				className='p-text'
+				name='name'
+				type='text'
+				placeholder='Your name'
+				value={name}
+				onChange={handleChangeInput}
+				required
+			/>
+		</div>
+		<div className='app__flex'>
+			<input
+				className={`${danger ? 'danger' : 'p-text'}`}
+				name='email'
+				type='email'
+				placeholder='Your email'
+				value={email}
+				onChange={handleChangeInput}
+				required
+			/>
+			{emailError && danger && (
+				<div className='app__flex'>
+					<p className='text-danger'>{emailError}</p>
+				</div>
 			)}
+		</div>
+		<div>
+			<textarea
+				name='message'
+				value={message}
+				onChange={handleChangeInput}
+				placeholder='Your Message'
+				className='p-text'
+				required
+			/>
+		</div>
+
+		<button type='submit' className='p-text' onClick={handleSubmit}>
+			{!loading ? 'Send Message' : 'Sending...'}
+		</button>
+	</form>
+) : (
+	<h3 className='thankyou'>Thankyou for getting in touch</h3>
+)}
     </>
   );
 };

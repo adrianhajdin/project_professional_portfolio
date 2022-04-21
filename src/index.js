7bdb59f96d9267e08cef686b38f090e092e731dc
Devanarayan J <53635547+devan-MEC@users.noreply.github.com>
2022-04-21 20:42:42 +0530

    Updated index.js to React 18 syntax

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './App';
-import './index.css';
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);

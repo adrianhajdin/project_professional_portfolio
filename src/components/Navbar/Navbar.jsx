1b7d2ea98c733b79d8e820054afb7f04cfd0acfc
onionSequences <kirbi9@gmail.com>
2022-06-15 12:12:53 +0200

    fix: removed anonymous function

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import { HiMenuAlt4, HiX } from 'react-icons/hi';
-import { motion } from 'framer-motion';
+import React, { useState } from "react";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
 
-import { images } from '../../constants';
-import './Navbar.scss';
+import { images } from "../../constants";
+import "./Navbar.scss";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
-  const handleOnClick = (e) => {
+  const handleOnClick = e => {
     e.stopPropagation();
-    setToggle((prevState) => !prevState);
+    setToggle(prevState => !prevState);
   };
 
   return (
@@ -19,7 +19,7 @@ const Navbar = () => {
         <img src={images.logo} alt="logo" />
       </div>
       <ul className="app__navbar-links">
-        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+        {["home", "about", "work", "skills", "contact"].map(item => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <div />
             <a href={`#${item}`}>{item}</a>
@@ -28,23 +28,23 @@ const Navbar = () => {
       </ul>
 
       <div className="app__navbar-menu">
-        <HiMenuAlt4 onClick={(e) => handleOnClick(e)} />
+        <HiMenuAlt4 onClick={handleOnClick} />
 
         {toggle && (
           <motion.div
             initial={{ width: 0 }}
             animate={{ width: 300 }}
-            transition={{ duration: 0.85, ease: 'easeOut' }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
           >
             <motion.span
               initial={{ width: 0 }}
               animate={{ width: 70 }}
-              transition={{ duration: 0.85, ease: 'easeOut' }}
+              transition={{ duration: 0.85, ease: "easeOut" }}
             >
-              <HiX onClick={(e) => handleOnClick(e)} />
+              <HiX onClick={handleOnClick} />
             </motion.span>
             <ul>
-              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+              {["home", "about", "work", "skills", "contact"].map(item => (
                 <li key={item}>
                   <a href={`#${item}`} onClick={() => setToggle(false)}>
                     {item}

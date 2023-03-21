ac902e13d5be76baaf78d565eea1df30b3b9152b
Gaurav Lohar <114788180+lohargaurav00@users.noreply.github.com>
2023-03-21 19:03:33 +0530

    Update Skills.jsx
    changed all tooltip code from v4 to v5 added a additional fixed a tooltip bug from small screen devices 
    
    additional code :- 
    used external react-responsive library  
    for tooltip position improvement

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -1,38 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
-import ReactTooltip from 'react-tooltip';
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { Tooltip as ReactTooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
+import { useMediaQuery } from "react-responsive";
 
-import { AppWrap, MotionWrap } from '../../wrapper';
-import { urlFor, client } from '../../client';
-import './Skills.scss';
+import "./Skills.scss";
+import { urlFor, client } from "../../Client";
+import { AppWrap, MotionWrap } from "../../Wrapper";
 
 const Skills = () => {
   const [experiences, setExperiences] = useState([]);
   const [skills, setSkills] = useState([]);
+  const isMobile = useMediaQuery({ query: "(max-width:700px)" });
 
   useEffect(() => {
     const query = '*[_type == "experiences"]';
     const skillsQuery = '*[_type == "skills"]';
-
-    client.fetch(query).then((data) => {
-      setExperiences(data);
-    });
-
-    client.fetch(skillsQuery).then((data) => {
-      setSkills(data);
-    });
+    client.fetch(query).then((data) => setExperiences(data));
+    client.fetch(skillsQuery).then((data) => setSkills(data));
   }, []);
-
   return (
     <>
-      <h2 className="head-text">Skills & Experiences</h2>
-
+      <h2 className="head-text">
+        Skills & <span>Experience</span>
+      </h2>
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
-          {skills.map((skill) => (
+          {skills?.map((skill) => (
             <motion.div
               whileInView={{ opacity: [0, 1] }}
-              transition={{ duration: 0.5 }}
+              transition={{ duration: 0.25 }}
               className="app__skills-item app__flex"
               key={skill.name}
             >
@@ -46,50 +43,44 @@ const Skills = () => {
             </motion.div>
           ))}
         </motion.div>
-        <div className="app__skills-exp">
-          {experiences.map((experience) => (
-            <motion.div
-              className="app__skills-exp-item"
-              key={experience.year}
-            >
+        <motion.div className="app__skills-exp">
+          {experiences?.map((experience) => (
+            <motion.div className="app__skills-exp-item" key={experience.year}>
               <div className="app__skills-exp-year">
                 <p className="bold-text">{experience.year}</p>
               </div>
               <motion.div className="app__skills-exp-works">
-                {experience.works.map((work) => (
-                  <>
+                {experience.works?.map((work) => (
+                  <div className="app__skills-tooltip" key={work.name}>
                     <motion.div
                       whileInView={{ opacity: [0, 1] }}
-                      transition={{ duration: 0.5 }}
+                      transition={{ duration: 0.25 }}
+                      data-tooltip-id={work.name}
                       className="app__skills-exp-work"
-                      data-tip
-                      data-for={work.name}
-                      key={work.name}
                     >
                       <h4 className="bold-text">{work.name}</h4>
                       <p className="p-text">{work.company}</p>
                     </motion.div>
                     <ReactTooltip
                       id={work.name}
-                      effect="solid"
-                      arrowColor="#fff"
+                      place={isMobile ? "top" : "right"}
                       className="skills-tooltip"
                     >
                       {work.desc}
                     </ReactTooltip>
-                  </>
+                  </div>
                 ))}
               </motion.div>
             </motion.div>
           ))}
-        </div>
+        </motion.div>
       </div>
     </>
   );
 };
 
 export default AppWrap(
-  MotionWrap(Skills, 'app__skills'),
-  'skills',
-  'app__whitebg',
+  MotionWrap(Skills, "app__skills"),
+  "skills",
+  "app__whitebg"
 );
